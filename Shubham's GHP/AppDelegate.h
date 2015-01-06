d0f869545067ab588ace3feec6f5711260ef7a3d
SylvrG <Shubham@Shubhams-MacBook-Pro.local>
2015-01-05 21:37:15 -0500

    Initial Commit

---
+++ Shubham's GHP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shubham's GHP
+//
+//  Created by Shubham Gupte on 1/5/15.
+//  Copyright (c) 2015 Sylvr Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Shubham's GHP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Shubham's GHP
+//
+//  Created by Shubham Gupte on 1/5/15.
+//  Copyright (c) 2015 Sylvr Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
